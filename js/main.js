2568dc07ff97a8dfdf9a33a26b14aa3b7d556520
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-12-10 14:16:39 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var balano = "Right on maaaan!";
+console.log(balano);
+
+var richie = "Don't let your meatloaf"
+console.log(typeof(richie));
+
+console.log(balano + " and then rich said:" + richie);
+
+var boo = true;
+console.log(typeof(boo));
+
+var var1 = undefined;
+console.log(typeof(var1));
+
+var var2 = NaN;
+console.log(typeof(var2));
+
+var var3 = null;
+console.log(typeof(var3));
